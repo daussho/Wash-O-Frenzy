30277507d7951dd154f4fbbfc9b522ea924b603b
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2018-04-22 17:26:19 +0700

    add pause

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+	private static int speed = 0;
+
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.back, speed * Time.deltaTime);
+	}
+
+	public void UpSpeed(){
+		speed += Random.Range(0, 100);
+	}
+
+	public void DownSpeed(){
+		speed -= Random.Range(0, 100);;
+	}
+}
